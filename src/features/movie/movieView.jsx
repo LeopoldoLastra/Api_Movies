c9839fc0ead2726c87bc22cfd96feddab91a9499
Gadd88 <matiassaade@gmail.com>
2023-11-17 23:23:04 -0300

    arreglados favoritos y localStorage

--- src/features/movie/movieView.jsx
+++ src/features/movie/movieView.jsx
@@ -3,55 +3,51 @@ import { HiOutlineUser, HiSearch, HiMenu } from "react-icons/hi";
 import { Link } from 'react-router-dom';
 import { Carrousel } from '../../core/components/carrousel/carrousel';
 import { useTmdb } from '../../core/services/useTmdb';
-import { IoBookmarkOutline, IoShareSocialOutline } from 'react-icons/io5';
+import { IoBookmarkOutline, IoBookmarkSharp ,IoShareSocialOutline } from 'react-icons/io5';
 import { BiLike } from 'react-icons/bi';
 import './movieView.css'
 import Header from "../../core/components/header/header";
+import { useContext } from "react";
+import { MoviesContext } from "../../context/MoviesContext";
 
 
 const MovieView = ()=>{
+
+  const {handleFavs, isFav} = useContext(MoviesContext)
 
   const movieId = (location.hash.split('/'))[2]
 
   const {information, error}=useTmdb({movieId})
 
+  const selectedMovie = { 
+    id:information.id,
+    title:information.title,
+    img:information.poster_path,
+    description:information.overview
+  }
+  const isFavMovie = isFav(movieId)
+  
 
 
-  const saveMovie = (e)=>{
+  const saveMovie = ()=>{
+    handleFavs(selectedMovie)
 
-    console.log('Si')
-
-    const selectedMovie = { id:information.id,
-        title:information. title,
-        img:information.poster_path,
-        description:information.overview
-      }
-
-    const persistentMovieList =[]
+    // const persistentMovieList =[]
  
-    if(!localStorage.getItem('Codo_Movie')){
+    // if(!localStorage.getItem('Codo_Movie')){
+    //   persistentMovieList.push(selectedMovie)
+    //   localStorage.setItem(`Codo_Movie`,JSON.stringify(persistentMovieList))
+    // }else{
+    //   const oldMovieList = JSON.parse(localStorage.getItem('Codo_Movie'))
+    //   oldMovieList.forEach((movie)=>{persistentMovieList.push(movie)})
+    //   persistentMovieList.push(selectedMovie)
+    //   const nueva = new Map([])
+    //   persistentMovieList.forEach((e)=>{
+    //     nueva.set(e.id, {id:e.id, title:e.title, img:e.img, overview:e.overview})
+    //   })
+    //   localStorage.setItem(`Codo_Movie`,JSON.stringify(persistentMovieList))
 
-      persistentMovieList.push(selectedMovie)
-      localStorage.setItem(`Codo_Movie`,JSON.stringify(persistentMovieList))
-   
-    }else{
-      
-      const oldMovieList = JSON.parse(localStorage.getItem('Codo_Movie'))
-      
-      oldMovieList.forEach((movie)=>{persistentMovieList.push(movie)})
-    
-      persistentMovieList.push(selectedMovie)
-
-      const nueva = new Map([])
-
-      persistentMovieList.forEach((e)=>{
-        nueva.set(e.id, {id:e.id, title:e.title, img:e.img, overview:e.overview})
-      })
-      
-      
-      localStorage.setItem(`Codo_Movie`,JSON.stringify(persistentMovieList))
-
-    }
+    // }
  
   }
 
@@ -59,33 +55,26 @@ const MovieView = ()=>{
   <>
    <>
       <Header/>
-      
       <main className='body' >
-     
-
-      <img src={`https://image.tmdb.org/t/p/original${information.poster_path}`}/>
-      
-      
-      <section className='body_action_bar'>
-      <BiLike className='icon'/>
-      <IoBookmarkOutline className='icon' onClick={()=>{saveMovie()}}/>
-      <IoShareSocialOutline className='icon' />
-      </section>
-
-
-
+        <img src={`https://image.tmdb.org/t/p/original${information.poster_path}`} width={300} height={300}/>
+        <section className='body_action_bar' style={{position: 'relative'}}>
+          <BiLike className='icon'/>
+          <span 
+            onClick={() => saveMovie()} 
+            className="icon">
+            {
+              isFavMovie.length>0 && <IoBookmarkSharp/> || <IoBookmarkOutline/>
+            }
+          </span>
+          <IoShareSocialOutline className='icon' />
+        </section>
         <section className='description_container'>
-          
             <div className='detail_container'>
               <h1>{information?.title}</h1>
               <p>{information?.overview}</p>
               <p>Reparto</p>
             </div>
-
         </section>
-
-
-
         <section className='carrousel_container'>
           <h2>Películas Relacionadas</h2> 
             <div className='cards_container'>
@@ -95,13 +84,11 @@ const MovieView = ()=>{
             </div>
         </section>       
       </main>
-
       <footer className='footer'>
         <ul>
           <li>Quienes Somos</li>
           <li>Contactanos</li>
         </ul>
-
       </footer>
     </>
   </>
